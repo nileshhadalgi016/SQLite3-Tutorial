746c1242b17dd85d30e3b6f9d967636c48914827
Nilesh Hadalgi <57908107+nileshhadalgi016@users.noreply.github.com>
2021-01-03 11:17:55 +0530

    Create SQLite3-Python-tutorial.py

---
+++ SQLite3-Python-tutorial.py
@@ -0,0 +1,28 @@
+
+# what is Database ??
+
+"""
+- A database is an organized collection of structured information, or data,
+typically stored electronically in a computer system. A database is usually controlled
+by a database management system (DBMS). Together, the data and the DBMS, along with
+the applications that are associated with them, are referred to as a database system,
+often shortened to just database.
+
+Data within the most common types of databases in operation today is typically
+modeled in rows and columns in a series of tables to make processing and data querying
+efficient. The data can then be easily accessed, managed, modified, updated, controlled,
+and organized. Most databases use structured query language (SQL) for writing and querying data.
+
+"""
+
+# What is Structured Query Language (SQL)?
+"""
+
+- SQL is a programming language used by nearly all relational databases to query,
+manipulate, and define data, and to provide access control. SQL was first developed
+at IBM in the 1970s with Oracle as a major contributor, which led to implementation
+of the SQL ANSI standard, SQL has spurred many extensions from companies such as IBM,
+Oracle, and Microsoft. Although SQL is still widely used today, new programming languages
+are beginning to appear.
+
+"""
